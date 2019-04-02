c2f0c9c23e399c145019947d35baa2746d0fe413
Tiago Bértolo <tiagobertolo@gmail.com>
2019-04-02 01:11:47 +0100

    First commit.

---
+++ src/Mongo.js
@@ -0,0 +1,68 @@
+const debug = require('debug')('simple-mongo-driver')
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+const ObjectID = mongodb.ObjectID
+
+var database, client, config, environment
+
+function onReconnect() {
+  debug('Mongo successfully reconnected!')
+}
+
+function onClose() {
+  debug('Mongo connection closed!')
+}
+
+function onConnect() {
+  debug('Mongo successfully connected at %O!', config.uri)
+}
+
+function isConnected() {
+  return !!client && !!client.topology && client.topology.isConnected()
+}
+
+function isValidObjectId(id) {
+  return ObjectID.isValid(id)
+}
+
+function getDatabase() {
+  return database
+}
+
+async function connect(inputEnvironment, inputConfig) {
+  if (isConnected()) {
+    return database
+  }
+  environment = inputEnvironment
+  config = inputConfig
+  debug('Connecting to database at %O...', config.uri)
+  client = await MongoClient.connect(config.uri, { useNewUrlParser: true })
+  onConnect()
+  client.on('close', onClose)
+  client.on('reconnect', onReconnect)
+  database = client.db(config.name)
+  return database
+}
+
+async function disconnect() {
+  if (isConnected()) {
+    debug('Disconnecting from database...')
+    await client.close()
+  }
+}
+
+async function dropDatabase() {
+  if (environment.toLowerCase() != 'production' && environment.toLowerCase() != 'prod') {
+    debug('WARNING! Dropping database!')
+    return database.dropDatabase()
+  }
+}
+
+module.exports = {
+  isValidObjectId,
+  getDatabase,
+  isConnected,
+  connect,
+  disconnect,
+  dropDatabase
+}
---
+++ test/Mongo.spec.js
@@ -0,0 +1,68 @@
+require('should')
+const _ = require('lodash')
+const Mongo = require('../src/Mongo')
+
+const TEST_CONFIG = {
+  environment: 'test',
+  database: {
+    uri: 'mongodb://localhost:27017',
+    name: 'test-database'
+  }
+}
+
+function isDatabase(db) {
+  let mongoDbKeys = [
+    'domain',
+    '_events',
+    '_eventsCount',
+    '_maxListeners',
+    's',
+    'serverConfig',
+    'bufferMaxEntries',
+    'databaseName'
+  ]
+  let dbKeys = Object.keys(db)
+  return _.isEqual(mongoDbKeys.sort(), dbKeys.sort())
+}
+
+describe('Mongo', () => {
+  it('isConnected should give false if not connected', async () => {
+    Mongo.isConnected().should.be.false()
+  })
+
+  it('should detect proper validate good ObjectId', () => {
+    Mongo.isValidObjectId('53fbf4615c3b9f41c381b6a3').should.be.true()
+  })
+
+  it('should detect invalid ObjectId', () => {
+    Mongo.isValidObjectId('53fbf461$c3b9f41c381b6a3').should.be.false()
+  })
+
+  it('should connect and disconnect to local Mongo', async () => {
+    Mongo.isConnected().should.be.false()
+    let db = await Mongo.connect(TEST_CONFIG.environment, TEST_CONFIG.database)
+    isDatabase(db).should.be.true()
+    isDatabase(Mongo.getDatabase()).should.be.true()
+    Mongo.isConnected().should.be.true()
+    await Mongo.disconnect()
+    Mongo.isConnected().should.be.false()
+  })
+
+  it('should succesfully drop database and check that is empty', async () => {
+    Mongo.isConnected().should.be.false()
+    let db = await Mongo.connect(TEST_CONFIG.environment, TEST_CONFIG.database)
+    isDatabase(db).should.be.true()
+    Mongo.isConnected().should.be.true()
+    let collection = db.collection('test-collection')
+    await collection.insertOne({ a: 1 })
+    let findResult = await collection.findOne({ a: 1 })
+    Mongo.isValidObjectId(findResult._id).should.be.true()
+    findResult.a.should.be.eql(1)
+    await Mongo.dropDatabase()
+    Mongo.isConnected().should.be.true()
+    let emptyFindResult = await collection.findOne({ a: 1 })
+    should.not.exist(emptyFindResult)
+    await Mongo.disconnect()
+    Mongo.isConnected().should.be.false()
+  })
+})
